191af7f1bd09dc705c4440ae58b8a0fb6be62335
ethan-000 <ethan_000@outlook.com>
2023-05-22 21:03:36 +0100

    add_halo2_backend

--- crates/nargo_cli/src/backends.rs
+++ crates/nargo_cli/src/backends.rs
@@ -1,9 +1,15 @@
-pub(crate) use acvm_backend_barretenberg::Barretenberg as ConcreteBackend;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "axiom_halo2_backend")] {
+        pub(crate) use halo2_backend::Halo2 as ConcreteBackend;
+    } else {
+        pub(crate) use acvm_backend_barretenberg::Barretenberg as ConcreteBackend;
+    }
+}
 
-#[cfg(not(any(feature = "plonk_bn254", feature = "plonk_bn254_wasm")))]
+#[cfg(not(any(feature = "plonk_bn254", feature = "plonk_bn254_wasm", feature = "halo2_backend")))]
 compile_error!("please specify a backend to compile with");
 
-#[cfg(all(feature = "plonk_bn254", feature = "plonk_bn254_wasm"))]
+#[cfg(all(feature = "plonk_bn254", feature = "plonk_bn254_wasm", feature = "halo2_backend"))]
 compile_error!(
     "feature \"plonk_bn254\"  and feature \"plonk_bn254_wasm\" cannot be enabled at the same time"
 );
