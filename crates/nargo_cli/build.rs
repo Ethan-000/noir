6eeeb799d7d73821da8d6fd80cc9ebe51ae76726
Ethan-000 <s2026080@ed.ac.uk>
2023-05-27 10:38:00 +0100

    commited on the wrong branch (#1)
    * switch rust toolchain
    
    * use feature

--- crates/nargo_cli/build.rs
+++ crates/nargo_cli/build.rs
@@ -1,6 +1,6 @@
 use rustc_version::{version, Version};
 
-fn check_rustc_version() {
+fn _check_rustc_version() {
     assert!(
         version().unwrap() >= Version::parse("1.66.0").unwrap(),
         "The minimal supported rustc version is 1.66.0."
@@ -10,7 +10,7 @@ fn check_rustc_version() {
 const GIT_COMMIT: &&str = &"GIT_COMMIT";
 
 fn main() {
-    check_rustc_version();
+    // check_rustc_version();
 
     // Only use build_data if the environment variable isn't set
     // The environment variable is always set when working via Nix
